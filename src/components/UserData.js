90397b38f6c4f7fcc55a7446a8431e5d2017ec91
Kyle Groening <kylegroening@gmail.com>
2023-01-21 14:01:11 -0500

    Add UserData.js file to hold all entered data

---
+++ src/components/UserData.js
@@ -0,0 +1,35 @@
+export let UserData = {
+  personalInfo: {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@email.com',
+    links: {
+      link1: 'github.com/johndoe',
+    },
+  },
+  education: {
+    entry1: {
+      schoolName: 'MIT',
+      degree: 'MS Computer Science',
+      yearCompleted: '2015',
+    },
+  },
+  experience: {
+    job1: {
+      employer: 'Google',
+      yearStarted: '2015',
+      yearEnded: '2017',
+      accomplishments: {
+        1: 'Lorem1',
+        2: 'Lorem2',
+        3: 'Lorem3',
+      }
+    },
+  },
+  projects: {
+    project1: {
+      title: 'API project',
+      description: 'Created an API to hack into the matrix',
+    },
+  },
+}
