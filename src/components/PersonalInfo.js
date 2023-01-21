8f6b9da50390acd9644b86fff5ad46a41f764668
Kyle Groening <kylegroening@gmail.com>
2023-01-21 15:12:09 -0500

    Add function to edit input fields

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -9,15 +9,31 @@ export class PersonalInfo extends React.Component {
     }
     this.DisplayName = this.DisplayName.bind(this);
     this.ChangeState = this.ChangeState.bind(this);
+    this.EditDisplayName = this.EditDisplayName.bind(this);
+    this.DisplayOrEdit = this.DisplayOrEdit.bind(this);
   }
 
   ChangeState(e){
-    const target = e.target.id
-    console.log(target)
+    const target = e.target.dataset.edit
     this.setState({
       [target]: !this.state[target]
     })
-    console.log(this.state)
+  }
+
+  FormCancelBtn(e){
+    e.preventDefault()
+    this.ChangeState(e)
+  }
+
+  FormSubmit(e, data){
+    e.preventDefault()
+    const first = e.target.firstName.value
+    const last = e.target.lastName.value    
+    const email = e.target.email.value
+    data.firstName = first
+    data.lastName = last
+    data.email = email
+    this.ChangeState(e)
   }
 
   DisplayName({ data }) {
@@ -28,16 +44,46 @@ export class PersonalInfo extends React.Component {
           <span>{data.lastName}</span>
         </div>
         <div className="email">{data.email}</div>
-        <span id = "displayNameEdit" className="edit" onClick={e => this.ChangeState(e)}>✎</span>
+        <span data-edit = "displayNameEdit" className="edit" onClick={e => this.ChangeState(e)}>✎</span>
       </div>
     )
   }
 
-  render() {
+  EditDisplayName({ data }){
     return (
       <div>
-        <this.DisplayName  data = {UserData.personalInfo}/>
+        <form onSubmit={e=> this.FormSubmit(e, data)} data-edit = "displayNameEdit">
+          <div>
+            <label htmlFor="firstName">First: </label>
+            <input name='firstName' type="text" defaultValue={data.firstName} autoFocus/>
+          </div>
+          <div>
+            <label htmlFor="lastName">Last: </label>
+            <input name='lastName' type="text" defaultValue={data.lastName}/>
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input name='email' type="email" defaultValue={data.email}/>
+          </div>
+          <button data-edit = "displayNameEdit" type="submit">Submit</button>
+          <button data-edit = "displayNameEdit" onClick={e=>this.FormCancelBtn(e)}>Cancel</button>
+
+        </form>
       </div>
+    )
+  }
+
+  DisplayOrEdit(){
+    if(this.state.displayNameEdit){
+      return <this.EditDisplayName data = {UserData.personalInfo}/>
+    } else {
+      return <this.DisplayName data = {UserData.personalInfo}/>
+    }
+  }
+
+  render() {
+    return (
+      <this.DisplayOrEdit />
     )
   }
 }
